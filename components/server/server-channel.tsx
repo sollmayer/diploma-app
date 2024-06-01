a43b9a431173e68beb802435b439948696222198
sollmayer <stupanenko.serhii@gmail.com>
2024-06-01 21:02:24 +0300

    add edit channel functionality

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -27,19 +27,21 @@ export const ServerChannel = ({channel, server, role}: ServerChannelProps) => {
 
     const Icon = iconMap[channel.type]
     return (
-        <button onClick={()=>{}} className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
-                                                params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
+        <button 
+        onClick={()=>{}} 
+        className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+                       params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
         )}>
             <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
-            <p className={cn("line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-                            params?.channelId === channel.id && "text-primary dark: text-zinc-200 dark:group-hover:text-white"
+            <p className={cn("line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 dark:group-hover:max-w-[7.5rem] overflow-hidden transition",
+                            params?.channelId === channel.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
             )}>
-            {channel.name}
+                {channel.name}
             </p>
             {channel.name !== "general" && role !== MemberRole.GUEST && (
                 <div className="ml-auto flex items-center gap-x-2" >
                     <ActionTooltip label="Edit">
-                        <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+                        <Edit onClick={()=>onOpen("editChannel", {server, channel})} className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
                     </ActionTooltip>
                     <ActionTooltip label="Delete">
                         <Trash onClick={()=>onOpen("deleteChannel", {server, channel})} className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,10 +1,9 @@
 import { Channel, ChannelType, Server } from "@prisma/client";
 import {create} from "zustand";
 
-export type ModalType = "createServer" | "invite" |
-                         "editServer" | "members" | 
-                         "createChannel" | "leaveServer" | 
-                         "deleteServer" | "deleteChannel";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | 
+                        "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" |
+                        "editChannel";
 
 interface ModalData {
     server?: Server;
