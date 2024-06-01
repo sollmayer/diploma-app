73dafc11e29f350e0be53d496a4a01cd745d0de0
sollmayer <stupanenko.serhii@gmail.com>
2024-06-01 15:38:41 +0300

    add server channels

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,4 +1,4 @@
-import { Server } from "@prisma/client";
+import { ChannelType, Server } from "@prisma/client";
 import {create} from "zustand";
 
 export type ModalType = "createServer" | "invite" |
@@ -7,7 +7,8 @@ export type ModalType = "createServer" | "invite" |
                          "deleteServer";
 
 interface ModalData {
-    server?: Server
+    server?: Server;
+    channelType?: ChannelType
 }
 
 interface ModalStore {
