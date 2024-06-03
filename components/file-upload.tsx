f28c2a94e12d842b351112b81b8afbfa265e33ce
sollmayer <stupanenko.serhii@gmail.com>
2024-06-03 09:54:48 +0300

    add file upload functionality

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -3,7 +3,7 @@
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css"
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 interface FileUploadProps {
@@ -26,6 +26,23 @@ export const FileUpload = ({
                     onClick={()=>onChange("")} 
                     type="button" 
                     className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm">
+                <X className="h-4 w-4" />
+                </button>
+            </div>
+        )
+    }
+
+    if(value && fileType === "pdf") {
+        return (
+            <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+                <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+                <a href={value} target="_blank" rel="noopener noreferrer" className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline">
+                    {value}
+                </a>
+                <button 
+                    onClick={()=>onChange("")} 
+                    type="button" 
+                    className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm">
                 <X className="h-4 w-4" />
                 </button>
             </div>
